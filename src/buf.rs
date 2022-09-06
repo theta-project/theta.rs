a25d923dec416d35d86de51a60896a5bb4ff6c5e
Maya Copeland <maya@tardisproject.uk>
2022-09-06 14:18:19 +0100

    finish buffer

--- src/buf.rs
+++ src/buf.rs
@@ -1,4 +1,4 @@
-use bytes::{BytesMut, BufMut};
+use bytes::{Buf, BufMut, BytesMut};
 
 pub struct Buffer {
     pub buffer: BytesMut,
@@ -68,7 +68,6 @@ impl Buffer {
 
         let mut uleb_len: usize = 0;
 
-
         while len > 0 {
             uleb[uleb_len] = (len as u8) & 0x7f;
 
@@ -79,7 +78,7 @@ impl Buffer {
 
             uleb_len += 1;
         }
-        
+
         uleb.retain(|&x| x != 0);
 
         self.buffer.put(uleb.as_slice());
@@ -92,9 +91,104 @@ impl Buffer {
         self.write_uleb(length);
         self.buffer.put(string.as_bytes());
     }
+
+    pub fn read_bool(&mut self) -> bool {
+        self.buffer.get_u8() != 0
+    }
+
+    pub fn read_u8(&mut self) -> u8 {
+        self.buffer.get_u8()
+    }
+
+    pub fn read_u16(&mut self) -> u16 {
+        self.buffer.get_u16_le()
+    }
+
+    pub fn read_u32(&mut self) -> u32 {
+        self.buffer.get_u32_le()
+    }
+
+    pub fn read_u64(&mut self) -> u64 {
+        self.buffer.get_u64_le()
+    }
+
+    pub fn read_i8(&mut self) -> i8 {
+        self.buffer.get_i8()
+    }
+
+    pub fn read_i16(&mut self) -> i16 {
+        self.buffer.get_i16_le()
+    }
+
+    pub fn read_i32(&mut self) -> i32 {
+        self.buffer.get_i32_le()
+    }
+
+    pub fn read_i64(&mut self) -> i64 {
+        self.buffer.get_i64_le()
+    }
+
+    pub fn read_float(&mut self) -> f32 {
+        self.buffer.get_f32_le()
+    }
+
+    pub fn read_double(&mut self) -> f64 {
+        self.buffer.get_f64_le()
+    }
+
+    fn read_uleb(&mut self) -> usize {
+        let mut result = 0;
+        let mut shift = 0;
+
+        let mut byte = self.read_u8();
+
+        if (byte & 0x80) == 0 {
+            result |= (byte & 0x7f) << shift;
+        } else {
+            let mut end = false;
+
+            while !end {
+                if shift > 0 {
+                    byte = self.read_u8();
+                }
+
+                result |= (byte & 0x7f) << shift;
+                if (byte & 0x80) == 0 {
+                    end = true;
+                }
+                shift += 7;
+            }
+        }
+
+        result as usize
+    }
+
+    pub fn read_str(&mut self) -> String {
+        let marker = self.read_u8();
+        let length = self.read_uleb();
+        let mut string = "".to_string();
+
+        if length > 0 {
+            let mut i = 0;
+            while i < length+1 {
+                let current_char = self.buffer.get(i);
+                let chr = match current_char {
+                    Some(x) => *x as char,
+                    None => '\0'
+                };
+
+                string = format!("{}{}", string, chr); //;
+                i += 1;
+            }
+
+            
+        }
+         
+        string
+    }
 }
 
-/* 
+/*
 impl BanchoLogin {
     pub fn write_to_buffer(&self, buf: &mut Buffer) {
         // push the data
@@ -104,6 +198,3 @@ impl BanchoLogin {
         buf.buffer.put_u32_le(self.id);
     }
 }*/
-
-
-
